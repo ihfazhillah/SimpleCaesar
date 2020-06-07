cc5400f80bc02f9593f4448522d2222f42eb7c1f
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-06-07 19:57:44 +0700

    Initial commit

---
+++ src/ihfazh/com/Main.java
@@ -0,0 +1,15 @@
+package ihfazh.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String myString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // when encrypt:
+        // a = d
+        // Z = c
+        // when decrypt:
+        // d = a
+        // c = Z
+    }
+}
